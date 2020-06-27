cb4c2805208f37f6b9b217187807453c0dad6658
Tyler Williams <tyler.williams.147@gmail.com>
2020-06-27 07:24:37 -0500

    Add project files.

---
+++ Clock/ClockConfiguration.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Clock
+{
+    public class ClockConfiguration : INotifyPropertyChanged
+    {
+        private double secondsAngle = 0;
+        public double SecondsAngle
+        {
+            get
+            {
+                return secondsAngle;
+            }
+            set
+            {
+                if (value != secondsAngle)
+                {
+                    secondsAngle = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private double minutessAngle = 0;
+        public double MinutesAngle
+        {
+            get
+            {
+                return minutessAngle;
+            }
+            set
+            {
+                if (value != minutessAngle)
+                {
+                    minutessAngle = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private double hoursAngle = 0;
+        public double HoursAngle
+        {
+            get
+            {
+                return hoursAngle;
+            }
+            set
+            {
+                if (value != hoursAngle)
+                {
+                    hoursAngle = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockConfiguration()
+        {
+            SecondsAngle = GetSecondsAngle();
+            MinutesAngle = GetMinutesAngle();
+            HoursAngle = GetHoursAngle();
+        }
+        public static double GetSecondsAngle()
+        {
+            TimeSpan day = (DateTime.Now - DateTime.Now.Date);
+            return (day.Seconds + day.Milliseconds / 1000.0) * 360 / 60.0;
+        }
+        public static double GetMinutesAngle()
+        {
+            TimeSpan day = (DateTime.Now - DateTime.Now.Date);
+            return (day.Minutes + (day.Seconds + day.Milliseconds / 1000.0) / 60.0) * 360 / 60.0;
+        }
+        public static double GetHoursAngle()
+        {
+            TimeSpan day = (DateTime.Now - DateTime.Now.Date);
+            return (day.Hours % 12) * 360 / 12.0 + (day.Minutes + (day.Seconds + day.Milliseconds / 1000.0) / 60.0) / 60.0 * 360 / 12.0;
+        }
+    }
+}
---
+++ Clock/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer timer = new Timer(1);
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            Dispatcher.Invoke(() =>
+            {
+                CC.SecondsAngle = ClockConfiguration.GetSecondsAngle();
+                CC.MinutesAngle = ClockConfiguration.GetMinutesAngle();
+                CC.HoursAngle = ClockConfiguration.GetHoursAngle();
+                //double secondsAngle = (SecondsHand.RenderTransform as TransformGroup).Children.OfType<RotateTransform>().First().Angle;
+            });
+            timer.Start();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
